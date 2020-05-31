a559b2aa21d060cb024812a33657a973581de7cf
Granite Bagas <granitebagas28@gmail.com>
2020-05-31 20:04:35 +0700

    Add service api

--- src/views/pages/service/Service.js
+++ src/views/pages/service/Service.js
@@ -1,90 +1,159 @@
-import React from "react";
-import Header from "../../../components/custom/Header";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardBody,
-  Table,
-  Button,
-} from "reactstrap";
-import { history } from "../../../history";
+import React, { useState } from "react";
+import { Card, CardBody, Button, Spinner, Col, Row } from "reactstrap";
+import Cookies from "js-cookie";
+import { Trash2, Edit } from "react-feather";
+import DataTable from "react-data-table-component";
+import { toast } from "react-toastify";
 
-const dummyData = [
-  {
-    name: "Laravel",
-    icon: '<i class="fab fa-laravel"></i>',
-    description:
-      "I can make a website build with Laravel a PHP Framework. I have a lot of projects built with Laravel. I can make a website full of Laravel, or make Laravel as a backend for an API. If you are interesting, please contact me.",
-  },
-  {
-    name: "Node.js",
-    icon: '<i class="fab fa-node-js"></i>',
-    description:
-      "I can make a website build with Node.js a Javascript Framework. I have a lot of projects built with Node.js. I can make a website full of Node.js, and use EJS, HTML, Pug or Handlebars as a template, or make Node.js as a backend for an API. If you are interesting, please contact me.",
-  },
-];
+import Header from "../../../components/custom/Header";
+import { history } from "../../../history";
+import baseAxios, { useAxios } from "../../../utility/baseAxios";
+import { useAuthContext } from "../../../contexts/AuthContext";
+import CustomHeader from "../../../components/custom/Table/CustomHeader";
+import LoadingSpinner from "../../../components/@vuexy/spinner/Loading-spinner";
+import Error505 from "../../misc/505";
+import { notAuthenticated } from "../../../utility/helper";
 
 const Service = () => {
+  const [{ data, loading, error }, refetch] = useAxios("service", {
+    useCache: false,
+  });
+  const [value, setValue] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [loadingDelete, setLoadingDelete] = useState(false);
+  const { dispatch } = useAuthContext();
+  const authToken = Cookies.get("token");
+
   const handleAdd = () => {
     history.push("/service/modify");
   };
 
-  const handleDelete = (data) => {
-    console.log(data);
+  const handleDelete = async (data) => {
+    try {
+      setLoadingDelete(true);
+
+      const { data: dataDelete } = await baseAxios.delete(
+        `service/${data.id}`,
+        {
+          headers: { Authorization: `Bearer ${authToken}` },
+        }
+      );
+      toast.success(dataDelete.message);
+      refetch();
+      setLoadingDelete(false);
+    } catch (error) {
+      if (error.response.status === 401) {
+        notAuthenticated(dispatch);
+      } else {
+        toast.error("Something Wrong!");
+      }
+    }
   };
+
+  const handleFilter = (e) => {
+    let text = e.target.value;
+    let filter = filteredData;
+    setValue(text);
+
+    if (text.length) {
+      filter = data.data.filter((item) => {
+        let startsWithCondition =
+          item.name.toLowerCase().startsWith(text.toLowerCase()) ||
+          item.percentage.toLowerCase().startsWith(text.toLowerCase());
+        let includesCondition =
+          item.name.toLowerCase().includes(text.toLowerCase()) ||
+          item.percentage.toLowerCase().includes(text.toLowerCase());
+
+        if (startsWithCondition) {
+          return startsWithCondition;
+        } else if (!startsWithCondition && includesCondition) {
+          return includesCondition;
+        } else return null;
+      });
+      setFilteredData(filter);
+    }
+  };
+
+  const columns = [
+    {
+      name: "Service Name",
+      selector: "name",
+      sortable: true,
+      cell: (row) => <p className="text-bold-500 my-1">{row.name}</p>,
+    },
+    {
+      name: "Service Icon",
+      selector: "icon",
+      sortable: true,
+      cell: (row) => <p className="text-bold-500 my-1">{row.icon}</p>,
+    },
+    {
+      name: "Service Description",
+      selector: "desc",
+      sortable: true,
+      cell: (row) => <p className="text-bold-500 my-1">{row.desc}</p>,
+    },
+    {
+      name: "Action",
+      selector: "",
+      cell: (row) => (
+        <Row>
+          <Col md="6">
+            <Button.Ripple
+              color="success"
+              onClick={() => history.push("/service/modify", { service: row })}
+              className="btn-icon rounded-circle"
+            >
+              <Edit />
+            </Button.Ripple>
+          </Col>
+          <Col md="6">
+            <Button.Ripple
+              color="danger"
+              onClick={() => handleDelete(row)}
+              disabled={loadingDelete}
+              className="btn-icon rounded-circle"
+            >
+              {loadingDelete ? <Spinner color="white" size="sm" /> : <Trash2 />}
+            </Button.Ripple>
+          </Col>
+        </Row>
+      ),
+    },
+  ];
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (error) {
+    return <Error505 retry={refetch} />;
+  }
 
   return (
     <React.Fragment>
       <Header title="Service" />
 
       <Card>
-        <CardHeader>
-          <CardTitle>Service</CardTitle>
-          <Button.Ripple color="primary" onClick={handleAdd}>
-            Add Service
-          </Button.Ripple>
-        </CardHeader>
         <CardBody>
-          <Table className="table-hover-animation" striped responsive>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Service Name</th>
-                <th>Service Icon</th>
-                <th>Service Description</th>
-                <th colSpan={2}>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dummyData.map((data, index) => (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{data.name}</td>
-                  <td>{data.icon}</td>
-                  <td>{data.description}</td>
-                  <td>
-                    <Button.Ripple
-                      color="success"
-                      onClick={() =>
-                        history.push("/service/modify", { data: data })
-                      }
-                    >
-                      Edit
-                    </Button.Ripple>
-                  </td>
-                  <td>
-                    <Button.Ripple
-                      color="danger"
-                      onClick={() => handleDelete(data)}
-                    >
-                      Delete
-                    </Button.Ripple>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
+          <DataTable
+            title="Skill"
+            className="dataTable-custom"
+            data={value.length ? filteredData : data.data}
+            columns={columns}
+            pagination
+            striped
+            highlightOnHover
+            subHeader
+            subHeaderComponent={
+              <CustomHeader
+                value={value}
+                handleFilter={handleFilter}
+                label="Skill"
+                handleAdd={handleAdd}
+              />
+            }
+          />
         </CardBody>
       </Card>
     </React.Fragment>
--- src/views/pages/service/ServiceModify.js
+++ src/views/pages/service/ServiceModify.js
@@ -11,80 +11,116 @@ import {
   Col,
   Button,
 } from "reactstrap";
+import Cookies from "js-cookie";
+import { toast } from "react-toastify";
 
 import Header from "../../../components/custom/Header";
 import InputText from "../../../components/custom/Form/InputText";
 import SubmitButton from "../../../components/custom/Form/SubmitButton";
 import { history } from "../../../history";
+import baseAxios from "../../../utility/baseAxios";
+import { notAuthenticated } from "../../../utility/helper";
+import { useAuthContext } from "../../../contexts/AuthContext";
 
 const ServiceModify = () => {
+  const { dispatch } = useAuthContext();
+  const authToken = Cookies.get("token");
+
   const formSchema = Yup.object().shape({
     name: Yup.string().required("Required"),
     icon: Yup.string().required("Required"),
-    description: Yup.string().required("Required"),
+    desc: Yup.string().required("Required"),
   });
 
-  const data = history.location.state;
+  const param = history.location.state;
 
-  const handleSubmit = (values) => {
-    console.log(values);
+  const handleSubmit = async (values) => {
+    try {
+      if (param) {
+        values._method = "PUT";
+      }
+      const url = param ? `service/${param.service.id}` : "service";
+      const { data } = await baseAxios({
+        url: url,
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        data: values,
+      });
+
+      toast.success(data.message);
+      history.push("/service");
+    } catch (error) {
+      if (error.response.status === 401) {
+        notAuthenticated(dispatch);
+      } else {
+        toast.error("Something Wrong!");
+      }
+    }
   };
 
   return (
     <React.Fragment>
-      <Header title={data ? "Edit Service" : "Add Service"} />
+      <Header title={param ? "Edit Service" : "Add Service"} />
 
       <Card>
         <CardHeader>
-          <CardTitle>{data ? "Edit Service" : "Add Service"}</CardTitle>
+          <CardTitle>{param ? "Edit Service" : "Add Service"}</CardTitle>
         </CardHeader>
         <CardBody>
           <Formik
             initialValues={{
-              name: data ? data.data.name : "",
-              icon: data ? data.data.icon : "",
-              description: data ? data.data.description : "",
+              name: param ? param.service.name : "",
+              icon: param ? param.service.icon : "",
+              desc: param ? param.service.desc : "",
             }}
             validationSchema={formSchema}
             onSubmit={handleSubmit}
           >
-            <Form>
-              <Row>
-                <Col sm="12">
-                  <InputText
-                    name="name"
-                    type="text"
-                    label="Service Name"
-                    placeholder="Masukkan Nama Service"
-                  />
-                </Col>
-                <Col sm="12">
-                  <InputText
-                    name="icon"
-                    type="text"
-                    label="Service Icon"
-                    placeholder="Masukkan Icon Service"
-                  />
-                </Col>
-                <Col sm="12">
-                  <InputText
-                    name="description"
-                    type="textarea"
-                    label="Service Description"
-                    placeholder="Masukkan Deskripsi Service"
-                    rows="5"
-                  />
-                </Col>
-              </Row>
-              <Button.Ripple
-                className="mr-1"
-                color="warning"
-                onClick={() => history.goBack()}
-              >
-                Back
-              </Button.Ripple>
-              <SubmitButton color="primary" label="Submit" />
-            </Form>
+            {({ isSubmitting }) => (
+              <Form>
+                <Row>
+                  <Col sm="12">
+                    <InputText
+                      name="name"
+                      type="text"
+                      label="Service Name"
+                      placeholder="Masukkan Nama Service"
+                    />
+                  </Col>
+                  <Col sm="12">
+                    <InputText
+                      name="icon"
+                      type="text"
+                      label="Service Icon"
+                      placeholder="Masukkan Icon Service"
+                    />
+                  </Col>
+                  <Col sm="12">
+                    <InputText
+                      name="desc"
+                      type="textarea"
+                      label="Service Description"
+                      placeholder="Masukkan Deskripsi Service"
+                      rows="5"
+                    />
+                  </Col>
+                </Row>
+                <Button.Ripple
+                  className="mr-1"
+                  color="warning"
+                  onClick={() => history.goBack()}
+                >
+                  Back
+                </Button.Ripple>
+                <SubmitButton
+                  color="primary"
+                  label="Submit"
+                  isSubmitting={isSubmitting}
+                />
+              </Form>
+            )}
           </Formik>
         </CardBody>
       </Card>
