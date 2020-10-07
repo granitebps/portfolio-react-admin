b073777a09ccfb527558f73de6f25c2e2c55a654
Granite Bagas <granitebagas28@gmail.com>
2020-10-07 14:56:06 +0700

    :construction: wip: fix github ci and app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import AuthContextProvider from './contexts/AuthContext';
+import { store } from './redux/storeConfig/store';
+import Spinner from './components/@vuexy/spinner/Fallback-spinner';
+import { Layout } from './utility/context/Layout';
+
+const LazyApp = lazy(() => import('./App'));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <AuthContextProvider>
+        <Suspense fallback={<Spinner />}>
+          <Layout>
+            <LazyApp />
+          </Layout>
+        </Suspense>
+      </AuthContextProvider>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
