2e1d7f7119be40906db210f6b3826d688a961ab6
Granite Bagas <granite.sidjabat@sociolla.com>
2025-01-01 03:18:51 +0700

    feat: Use nextjs

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = (config) => {
-  require("react-app-rewire-postcss")(config, {
-    plugins: () => [require("postcss-rtl")()],
-  });
-
-  return config;
-};
--- src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import customizer from "./customizer/";
-
-const rootReducer = combineReducers({
-  customizer: customizer,
-});
-
-export default rootReducer;
